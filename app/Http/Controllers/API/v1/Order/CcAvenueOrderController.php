726bdf46ab00e42dc3359c361f4aab3b8f3e4485
Raiyan <raiyan.appdid@gmail.com>
2023-09-25 14:38:25 +0530

    updatd

--- app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
+++ app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\API\v1\Order;
 
 use App\Http\Controllers\Controller;
 use App\Models\CcAvenueOrder;
+use App\Services\ccavenue\PaymentService;
 use Illuminate\Http\Request;
+
+use function Termwind\render;
 
 class CcAvenueOrderController extends Controller
 {
@@ -33,9 +36,14 @@ class CcAvenueOrderController extends Controller
         $data->email = $request->email;
         $data->total_price = $request->total_price;
         $data->save();
-        return response([
-            'success' => true,
-            'message' => "Order Created",
-        ]);
+        // return response([
+        //     'success' => true,
+        //     'message' => "Order Created",
+        // ]);
+
+        $paymentService = new PaymentService;
+        $order = $paymentService->createOrder(1, "https://glosense.in/billing-details", "https://glosense.in/billing-details", $request->all());
+
+        return $order->rendered();
     }
 }
