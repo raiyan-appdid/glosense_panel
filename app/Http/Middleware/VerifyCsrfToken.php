451bfddcbfde7dc070d8286a46e23339862710a5
Raiyan <raiyan.appdid@gmail.com>
2023-10-03 14:25:17 +0530

    Callbacks Added

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/ccavenue/failed/*',
+        '/failed/*',
         '/ccavenue/success',
     ];
 }
