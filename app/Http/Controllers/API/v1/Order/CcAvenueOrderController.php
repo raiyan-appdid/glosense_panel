c6790dace473975f84f0774e0efda1bfdd895fc6
Raiyan <raiyan.appdid@gmail.com>
2023-10-03 14:43:39 +0530

    Callbacks Added

--- app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
+++ app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
@@ -47,7 +47,7 @@ class CcAvenueOrderController extends Controller
         $order = $paymentService->createOrder(
             amount: 1,
             redirect_url: route("ccavenue.success"),
-            cancel_url: route("ccavenue.failed"),
+            cancel_url: "",
             additional_data: ['billing_name' => $request->name, 'billing_tel' => $request->number, 'billing_email' => $request->email, 'address' => $request->billing_address, 'billing_zip' => $request->pincode, 'billing_tel' => $request->number, 'billing_city' => $request->city, 'billing_state' => $request->state, 'billing_country' => $request->country]
         );
         // return 'raiyan';
