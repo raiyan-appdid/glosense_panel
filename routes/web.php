0803b3a16871f00b56bca28a794c7dfa52261d86
Raiyan <raiyan.appdid@gmail.com>
2023-10-03 13:54:55 +0530

    Callbacks Added

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ use App\Models\ComboProduct;
 
 Route::prefix('ccavenue')->name('ccavenue.')->controller(BasicController::class)->group(function () {
     Route::get('success', 'successCallBack')->name('success');
-    Route::get('failed', 'failedCallBack')->name('failed');
+    Route::post('failed', 'failedCallBack')->name('failed');
 });
 
 
