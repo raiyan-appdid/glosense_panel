7c048c89d495acb82ef7c06a90b0e5853617f647
Raiyan <raiyan.appdid@gmail.com>
2023-10-06 17:22:47 +0530

    Major Update

--- app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
+++ app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
@@ -60,7 +60,7 @@ class CcAvenueOrderController extends Controller
             amount: 1,
             redirect_url: route("ccavenue.success"),
             cancel_url: route("ccavenue.failed"),
-            additional_data: ['billing_name' => $request->name, 'billing_tel' => $request->number, 'billing_email' => $request->email, 'address' => $request->addresss, 'billing_zip' => $request->pincode, 'billing_tel' => $request->number, 'billing_city' => $request->city, 'billing_state' => $request->state, 'billing_country' => $request->country],
+            additional_data: ['billing_name' => $request->name, 'billing_tel' => $request->number, 'billing_email' => $request->email, 'billing_address' => $request->addresss, 'billing_zip' => $request->pincode, 'billing_tel' => $request->number, 'billing_city' => $request->city, 'billing_state' => $request->state, 'billing_country' => $request->country],
             CCAvenueorderId: $CCAvenueorderId,
         );
         // return 'raiyan';
