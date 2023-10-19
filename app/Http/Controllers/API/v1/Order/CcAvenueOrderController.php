bb414df11d13e68f5dafc41b14eb3e47be4c3380
Raiyan <raiyan.appdid@gmail.com>
2023-10-19 11:31:08 +0530

    updated

--- app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
+++ app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
@@ -48,10 +48,10 @@ class CcAvenueOrderController extends Controller
         $data->units = 1;
 
         $code = Promocode::where('promocode', $request->promocode)->first();
-        return $code;
         if ($code) {
             $data->price = 899 - $code->discount;
             $data->sub_total = 899  - $code->discoun;
+            return 899 - $code->discount;
         } else {
             $data->price = 899;
             $data->sub_total = 899;
