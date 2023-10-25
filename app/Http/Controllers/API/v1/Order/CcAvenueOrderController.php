61c1d8cf3aa2262e975f09646218d1e54616d5ab
Raiyan <raiyan.appdid@gmail.com>
2023-10-25 17:34:00 +0530

    updated

--- app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
+++ app/Http/Controllers/API/v1/Order/CcAvenueOrderController.php
@@ -18,7 +18,7 @@ class CcAvenueOrderController extends Controller
 {
     public function store(Request $request)
     {
-     $request->all();   
+    return $request->all();   
         $request->validate([
             // 'name' => 'required',
             // 'address' => 'required',
