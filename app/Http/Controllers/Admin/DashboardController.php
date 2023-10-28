a66a98bf487ea17b0408989e648f5d922137a9d9
Raiyan Memon <raiyan.appdid@gmail.com>
2023-10-28 13:00:00 +0530

    updated

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,6 +9,7 @@ use App\Models\Product;
 use App\Models\Promocode;
 use App\Models\Testimonial;
 use App\Models\User;
+use App\Services\ccavenue\PaymentService;
 use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
@@ -16,6 +17,12 @@ class DashboardController extends Controller
 
     public function home()
     {
+
+        $paymentService = new PaymentService();
+        $paymentService->fetchOrder('0044c44b-0855-47f6-b3dc-8939068f73e8');
+        return $paymentService;
+
+
         $users = User::withoutadmin()->count();
         $products = Product::count();
         $enquiry = Enquiry::count();
