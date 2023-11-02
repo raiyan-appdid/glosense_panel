d50457123fdcbd74b85bd7a8b06f015003493b28
Raiyan <raiyan.appdid@gmail.com>
2023-11-02 11:53:19 +0530

    updated

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -26,33 +26,33 @@ class DashboardController extends Controller
     {
 
 
-        $this->workingKey = env('CC_WORKING_KEY');
-        $this->accessCode = env('CC_ACCESS_CODE');
-        $this->merchantId = env('CC_MERCHANT_ID');
-        $this->language = "EN";
-        $this->currency = "INR";
-        $this->version = 1.1;
+        // $this->workingKey = env('CC_WORKING_KEY');
+        // $this->accessCode = env('CC_ACCESS_CODE');
+        // $this->merchantId = env('CC_MERCHANT_ID');
+        // $this->language = "EN";
+        // $this->currency = "INR";
+        // $this->version = 1.1;
 
-        $merchant_json_data = array(
-            'order_no' => 'ceb82bd2-a62a-453c-a5d3-bef0230f26d0', //your app's internal order no
-            'reference_no' => '113062410532' //ccavenue tracking no
-        );
+        // $merchant_json_data = array(
+        //     'order_no' => 'ceb82bd2-a62a-453c-a5d3-bef0230f26d0', //your app's internal order no
+        //     'reference_no' => '113062410532' //ccavenue tracking no
+        // );
 
-        $merchant_data = json_encode($merchant_json_data);
-        $encrypted_data = encrypt($merchant_data, $this->workingKey);
+        // $merchant_data = json_encode($merchant_json_data);
+        // $encrypted_data = encrypt($merchant_data, $this->workingKey);
 
-        $response = Http::asForm()->withHeaders([
-            'Content-Type' => 'application/x-www-form-urlencoded',
-        ])->post('https://api.ccavenue.com/apis/servlet/DoWebTrans', [
-            'enc_request' => $encrypted_data,
-            'access_code' => $this->accessCode,
-            'command' => 'orderStatusTracker',
-            'request_type' => 'JSON',
-            'response_type' => 'JSON',
-            'version' => '1.3'
-        ])->body();
+        // $response = Http::asForm()->withHeaders([
+        //     'Content-Type' => 'application/x-www-form-urlencoded',
+        // ])->post('https://api.ccavenue.com/apis/servlet/DoWebTrans', [
+        //     'enc_request' => $encrypted_data,
+        //     'access_code' => $this->accessCode,
+        //     'command' => 'orderStatusTracker',
+        //     'request_type' => 'JSON',
+        //     'response_type' => 'JSON',
+        //     'version' => '1.3'
+        // ])->body();
 
-        dd($response);
+        // dd($response);
 
 
 
@@ -61,41 +61,43 @@ class DashboardController extends Controller
 
         
 
-        $command = "orderStatusTracker";
-        $final_data = "request_type=JSON&access_code=" . $this->accessCode . "&command=" . $command . "&response_type=JSON&version=" . $this->version;
+        // $command = "orderStatusTracker";
+        // $final_data = "request_type=JSON&access_code=" . $this->accessCode . "&command=" . $command . "&response_type=JSON&version=" . $this->version;
 
-        // return $final_data;
-
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://api.ccavenue.com/apis/servlet/DoWebTrans");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $final_data);
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        $information = explode('&', $result);
-        $dataSize = sizeof($information);
-        $status1 = explode('=', $information[0]);
-        $status2 = explode('=', $information[1]);
-        // $status3 = explode('=', $information[2]);
-        if ($status1[1] == '1') {
-            return [$information, $status1, $status2];
-            $recorddata = $status2[1];
-            return $recorddata . " Error Code:" . $status2[1];
-        } else {
-            // $status = self::decrypt($status2[1], $this->working_key);
-            return $status2;
-        }
+        // // return $final_data;
 
 
-        $data = new PaymentService;
-        $data->fetchOrder("3f8fe7df-a4b7-4c5e-8550-652fb1ebe95e");
-        return $data;
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, "https://api.ccavenue.com/apis/servlet/DoWebTrans");
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        // curl_setopt($ch, CURLOPT_POST, 1);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, $final_data);
+
+        // $result = curl_exec($ch);
+        // curl_close($ch);
+
+        // $information = explode('&', $result);
+        // $dataSize = sizeof($information);
+        // $status1 = explode('=', $information[0]);
+        // $status2 = explode('=', $information[1]);
+        // // $status3 = explode('=', $information[2]);
+        // if ($status1[1] == '1') {
+        //     return [$information, $status1, $status2];
+        //     $recorddata = $status2[1];
+        //     return $recorddata . " Error Code:" . $status2[1];
+        // } else {
+        //     // $status = self::decrypt($status2[1], $this->working_key);
+        //     return $status2;
+        // }
+
+
+        // $data = new PaymentService;
+        // $data->fetchOrder("3f8fe7df-a4b7-4c5e-8550-652fb1ebe95e");
+        // return $data;
+
+        Mail::to("raiyanmemon7860@gmail.com")->send(new Invoice('723'));
 
         $users = User::withoutadmin()->count();
         $products = Product::count();
--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Order Invoice</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style type="text/css">
-        /**
-   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
-   */
+        /* body {
+            font-family: "Rupee", Arial, sans-serif;
+        } */
         @media screen {
             @font-face {
                 font-family: 'Source Sans Pro';
@@ -27,296 +26,155 @@
             }
         }
 
-        /**
-   * Avoid browser level font resizing.
-   * 1. Windows Mobile
-   * 2. iOS / OSX
-   */
-        body,
-        table,
-        td,
-        a {
-            -ms-text-size-adjust: 100%;
-            /* 1 */
-            -webkit-text-size-adjust: 100%;
-            /* 2 */
+        @page {
+            size: A4;
         }
 
-        /**
-   * Remove extra space added to tables and cells in Outlook.
-   */
-        table,
-        td {
-            mso-table-rspace: 0pt;
-            mso-table-lspace: 0pt;
+        .invoice-container {
+            width: 680px;
+            margin: 0 auto;
+            padding: 10px;
+            border: 1px solid #ccc;
         }
 
-        /**
-   * Better fluid images in Internet Explorer.
-   */
-        img {
-            -ms-interpolation-mode: bicubic;
+        .logo {
+            text-align: center;
+            margin-bottom: 20px;
         }
 
-        /**
-   * Remove blue links for iOS devices.
-   */
-        a[x-apple-data-detectors] {
-            font-family: inherit !important;
-            font-size: inherit !important;
-            font-weight: inherit !important;
-            line-height: inherit !important;
-            color: inherit !important;
-            text-decoration: none !important;
+        .address-container {
+            display: table;
+            width: 100%;
+            margin-bottom: 20px;
         }
 
-        /**
-   * Fix centering issues in Android 4.4.
-   */
-        div[style*="margin: 16px 0;"] {
-            margin: 0 !important;
+        .address {
+            display: table-cell;
+            width: 50%;
         }
 
-        body {
-            width: 100% !important;
-            height: 100% !important;
-            padding: 0 !important;
-            margin: 0 !important;
+        .invoice-details {
+            margin-bottom: 20px;
         }
 
-        /**
-   * Collapse table borders to avoid space between cells.
-   */
-        table {
-            border-collapse: collapse !important;
+        .invoice-details table {
+            width: 100%;
+            border-collapse: collapse;
         }
 
-        a {
-            color: #1a82e2;
+        .invoice-details table td {
+            padding: 8px;
+            border: 1px solid #ccc;
         }
 
-        img {
-            height: auto;
-            line-height: 100%;
-            text-decoration: none;
-            border: 0;
-            outline: none;
+        .invoice-items {
+            margin-bottom: 20px;
+        }
+
+        .invoice-items table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        .invoice-items table th {
+            background-color: #f2f2f2;
+            padding: 8px;
+            border: 1px solid #ccc;
+            text-align: left;
+        }
+
+        .invoice-items table td {
+            padding: 8px;
+            border: 1px solid #ccc;
+        }
+
+        .total {
+            text-align: right;
         }
     </style>
-
 </head>
 
-<body style="background-color: #D2C7BA;">
+<body>
+    <div class="invoice-container">
+        <div class="logo">
+            <img src="https://glosense.in/images/logo.png" alt="Company Logo" width="150">
+        </div>
 
-    {{-- @dump($updateOrder) --}}
-    <!-- start preheader -->
-    <div class="preheader"
-        style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
-        Here is a summary of your recent order. If you have any questions or concerns about your order, please <a
-            href="https://glosense.in">contact us.</a>
+        <div class="address-container">
+            <div class="address">
+                <strong>From:</strong><br>
+                YCT Youth Publication<br>
+                12, Church Ln, The Adelphi, Allen Ganj, <br>
+                Prayagraj, Uttar Pradesh 211001
+
+            </div>
+            <div class="address">
+                <strong>To:</strong><br>
+                {{ $updateOrder->address }}
+            </div>
+        </div>
+
+        <div class="invoice-details">
+            <table>
+                <tr>
+                    <td>Customer Name</td>
+                    <td>{{ $updateOrder->name ?? '' }}</td>
+                </tr>
+                <tr>
+                    <td>Customer Mobile</td>
+                    <td>{{ $updateOrde->number ?? '' }}</td>
+                </tr>
+                <tr>
+                    <td>Invoice Number</td>
+                    <td>#{{ $updateOrder->id ?? '' }}</td>
+                </tr>
+                <tr>
+                    <td>Date</td>
+                    <td>{{ date('F d, Y', strtotime($updateOrder->created_at)) }} </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="invoice-items">
+            <table>
+                <tr>
+                    <th>Sr No</th>
+                    <th>Title</th>
+                    <th>Quantity</th>
+                    <th>Amount</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Hair You Glo</td>
+                    <td>{{ $updateOrder->units }}</td>
+                    <td>Rs. 1299</td>
+                </tr>
+                <tr>
+                    <td colspan="3">Subtotal:</td>
+                    <td>{{ 'Rs. ' . $updateOrder->price }}</td>
+                </tr>
+                {{-- <tr>
+                    <td colspan="3">Discount</td>
+                    <td>{{ 'Rs. ' . $updateOrder->discount }}</td>
+                </tr> --}}
+                <tr>
+                    <td colspan="3">Delivery Charges</td>
+                    <td>Rs. 0.00</td>
+                </tr>
+                <tr>
+                    <td colspan="3">Discount</td>
+                    <td>{{ 'Rs. ' . $updateOrder->discount }}</td>
+                </tr>
+                <tr>
+                    <td colspan="3">Total:</td>
+                    <td>{{ 'Rs. ' . $updateOrder->total }}</td>
+                </tr>
+            </table>
+            <div style="text-align: center;">
+                <p>This is a system-generated invoice. No physical signature is required.</p>
+            </div>
+        </div>
     </div>
-    <!-- end preheader -->
-
-    <!-- start body -->
-    <table border="0" cellpadding="0" cellspacing="0" width="100%">
-
-        <!-- start logo -->
-        <tr>
-            <td align="center" bgcolor="#D2C7BA">
-
-                {{-- <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                    <tr>
-                        <td align="center" valign="top" style="padding: 36px 24px;">
-                            <a href="https://maryammanemaddu.com/" target="_blank" style="display: inline-block;">
-                                <img src="https://maryammanemaddu.com/images/logo/favicon.ico" alt="Logo"
-                                    border="0" width="48"
-                                    style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
-                            </a>
-                        </td>
-                    </tr>
-                </table> --}}
-
-            </td>
-        </tr>
-        <!-- end logo -->
-
-        <!-- start hero -->
-        <tr>
-            <td align="center" bgcolor="#D2C7BA">
-
-                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                    <tr>
-                        <td align="left" bgcolor="#ffffff"
-                            style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-                            <h1
-                                style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">
-                                Thank you for your order!</h1>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <!-- end hero -->
-
-        <!-- start copy block -->
-        <tr>
-            <td align="center" bgcolor="#D2C7BA">
-
-                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-
-                    <!-- start copy -->
-                    <tr>
-                        <td align="left" bgcolor="#ffffff"
-                            style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                            <p style="margin: 0;">Here is a summary of your recent order. If you have any questions or
-                                concerns about your order, please <a href="https://glosense.in">contact
-                                    us</a>.</p>
-                        </td>
-                    </tr>
-                    <!-- end copy -->
-
-                    <!-- start receipt table -->
-                    <tr>
-                        <td align="left" bgcolor="#ffffff"
-                            style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                                <tr>
-                                    <td align="left" width="50%">
-                                        <strong>Order #</strong>
-                                    </td>
-                                    <td align="right" width="20%"
-                                        style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                        <strong>{{ $updateOrder->order_id }}</strong>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td align="left" bgcolor="#D2C7BA" width="50%"
-                                        style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                        <strong>Items #</strong>
-                                    </td>
-                                    <td align="left" bgcolor="#D2C7BA" width="20%"
-                                        style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                        <strong>Qty</strong>
-                                    </td>
-                                    <td align="left" bgcolor="#D2C7BA" width="30%"
-                                        style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;white-space: nowrap;">
-                                        <strong>Price</strong>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td align="left" width="50%"
-                                        style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                        {{ $updateOrder->product_name ?? '' }}</td>
-                                    <td align="left" width="20%"
-                                        style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                        {{ $updateOrder->units ?? '' }}</td>
-                                    <td align="left" width="30%"
-                                        style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; white-space: nowrap;">
-                                        ₹ {{ $updateOrder->price ?? '' }}</td>
-
-                                </tr>
-                                <tr>
-                                    <td align="left" width="75%"
-                                        style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                        Shipping</td>
-                                    <td align="left" width="20%"
-                                        style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                    </td>
-                                    <td align="left" width="25%"
-                                        style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; white-space: nowrap;">
-                                        ₹ 0</td>
-
-                                </tr>
-                                <tr>
-                                    <td align="left" width="75%"
-                                        style="padding: 12px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;">
-                                        <strong>Total</strong>
-                                    </td>
-                                    <td align="left" width="20%"
-                                        style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;">
-                                    </td>
-                                    <td align="left" width="25%"
-                                        style="padding: 12px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA; white-space: nowrap;">
-                                        <strong>₹ {{ $updateOrder->sub_total ?? '' }}</strong>
-                                    </td>
-
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <!-- end reeipt table -->
-
-                </table>
-
-            </td>
-        </tr>
-        <!-- end copy block -->
-
-        <!-- start receipt address block -->
-        <tr>
-            <td align="center" bgcolor="#D2C7BA" valign="top" width="100%">
-
-                <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%"
-                    style="max-width: 600px;">
-                    <tr>
-                        <td align="center" valign="top" style="font-size: 0; border-bottom: 3px solid #d4dadf">
-                            <div
-                                style="display: inline-block; width: 100%; max-width: 50%; min-width: 240px; vertical-align: top;">
-                                <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%"
-                                    style="max-width: 300px;">
-                                    <tr>
-                                        <td align="left" valign="top"
-                                            style="padding-bottom: 36px; padding-left: 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                            <p><strong>Delivery Address</strong></p>
-                                            <p>{{ $updateOrder->address}},<br>{{ $updateOrder->city }},<br>{{ $updateOrder->state }},
-                                                {{ $updateOrder->pincode ?? '' }}</p>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </div>
-                            <div
-                                style="display: inline-block; width: 100%; max-width: 50%; min-width: 240px; vertical-align: top;">
-                                {{-- <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%"
-                                    style="max-width: 300px;">
-                                    <tr>
-                                        <td align="left" valign="top"
-                                            style="padding-bottom: 36px; padding-left: 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                                            <p><strong>Billing Address</strong></p>
-                                            <p>{{ $delivery_address?->address_one ?? '' }},<br>{{ $delivery_address?->address_two ?? '' . ' ' ."vapi" ?? '' }},<br>{{ "state" ?? '' }},
-                                                {{ $delivery_address?->pincode ?? '' }}</p>
-                                        </td>
-                                    </tr>
-                                </table> --}}
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td align="center" bgcolor="#D2C7BA" style="padding: 24px;">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-
-                    <!-- start permission -->
-                    <tr>
-                        <td align="center" bgcolor="#D2C7BA"
-                            style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-                            <p style="margin: 0;">You received this email because we received a product request. If you
-                                didn't request product you can safely
-                                delete this email.</p>
-                        </td>
-                    </tr>
-                    <!-- end permission -->
-
-
-                </table>
-            </td>
-        </tr>
-        <!-- end footer -->
-
-    </table>
-    <!-- end body -->
-
 </body>
 
 </html>
