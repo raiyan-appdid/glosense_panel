1ec51a4ede54b0099c917009e1b227eb14106866
Raiyan <raiyan.appdid@gmail.com>
2024-01-29 18:07:56 +0530

    updated

--- app/Services/CashFree/CashFreePaymentService.php
+++ app/Services/CashFree/CashFreePaymentService.php
@@ -29,7 +29,6 @@ class CashFreePaymentService
     {
         try {
             $returnUrl = route('cashfree.callback') . "?order_id" . $orderId;
-            return $returnUrl;
             $response = Http::withHeaders([
                 'x-client-id' => $this->apiKey,
                 'x-client-secret' => $this->apiSecret,
