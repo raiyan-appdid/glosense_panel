d01bd3414d98d898283065537ffa6ae4412521ab
Raiyan <raiyan.appdid@gmail.com>
2024-02-26 12:41:34 +0530

    updateed

--- app/Console/Commands/CheckRazorpayPayment.php
+++ app/Console/Commands/CheckRazorpayPayment.php
@@ -2,8 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Models\CcAvenueTransaction;
 use App\Models\Order;
+use App\Services\RazorPayIntegration;
+use App\Services\ShipRocket\CreateOrderService;
+use App\Services\ShipRocket\GenerateTokenService;
 use Illuminate\Console\Command;
+use App\Mail\Invoice;
+use Illuminate\Support\Facades\Mail;
 
 class CheckRazorpayPayment extends Command
 {
@@ -29,16 +35,38 @@ class CheckRazorpayPayment extends Command
     public function handle()
     {
 
-        $OrdersWithStatusPending = Order::where('status', 'pending')->whereHas('transaction', function ($q) {
+        $OrdersWithStatusPending = Order::where('status', 'pending')->orderBy('id', 'desc')->whereHas('transaction', function ($q) {
             $q->where('payment_gateway', "razorpay live");
-        })->get();
+        })->first();
         \Log::info($OrdersWithStatusPending);
 
-        foreach ($OrdersWithStatusPending as  $value) {
-            if ($value->transaction->razorpay_order_id) {
-                \Log::info($value);
-            }
-        }
+        // foreach ($OrdersWithStatusPending as  $OrdersWithStatusPending) {
+        // if ($OrdersWithStatusPending->transaction->razorpay_order_id) {
+
+        //     $checkPayment = RazorPayIntegration::fetchOrder($OrdersWithStatusPending->transaction->razorpay_order_id);
+        //     if ($checkPayment['success']) {
+        //         $status = $checkPayment['status'];
+        //         $razorpayOrderData = CcAvenueTransaction::where('razorpay_order_id', $OrdersWithStatusPending->transaction->razorpay_order_id)->first();
+        //         $razorpayOrderData->status = $status;
+        //         $razorpayOrderData->save();
+        //         if ($status == 'paid' || $status == 'captured') {
+        //             $OrdersWithStatusPending->status = "PAID";
+        //             $transactionStatus = CcAvenueTransaction::where('id', $OrdersWithStatusPending->transaction->id)->with(['order'])->first();
+        //             $transactionStatus->status = 'PAID';
+        //             $transactionStatus->save();
+
+        //             $token = new GenerateTokenService;
+        //             $token = $token->getToken();
+        //             $shiprocketOrder = new CreateOrderService;
+        //             $response = $shiprocketOrder->create($token, $OrdersWithStatusPending);
+        //             $OrdersWithStatusPending->shiprocket_order_id = $response['order_id'];
+        //             $OrdersWithStatusPending->shipment_id = $response['shipment_id'];
+        //             $OrdersWithStatusPending->save();
+        //             Mail::to($OrdersWithStatusPending->email)->send(new Invoice($transactionStatus->order->id));
+        //         }
+        //     }
+        // }
+        // }
 
 
 
