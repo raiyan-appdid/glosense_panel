128f99430f960f8dd695113c8a48839df1c57378
Raiyan <raiyan.appdid@gmail.com>
2024-03-04 15:17:56 +0530

    updated

--- app/DataTables/AnalyticDataTable.php
+++ app/DataTables/AnalyticDataTable.php
@@ -45,7 +45,7 @@ class AnalyticDataTable extends DataTable
      */
     public function query(Analytic $model)
     {
-        return $model->newQuery();
+        return $model->with(['user'])->newQuery();
     }
 
     /**
@@ -86,7 +86,7 @@ class AnalyticDataTable extends DataTable
     protected function getColumns()
     {
         return [
-            Column::make('id'), Column::make('user_id'),
+            Column::make('id'), Column::make('user_id', 'user.first_name'),
             Column::make('created_at'),
             Column::computed('action')
                 ->exportable(false)
