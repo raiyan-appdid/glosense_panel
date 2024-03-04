26c688c4ee428e2aec89fa29d5cf434ab05620ea
Raiyan <raiyan.appdid@gmail.com>
2024-03-04 15:03:44 +0530

    updated

---
+++ database/migrations/2024_03_04_125145_create_analytics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('analytics', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('for')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('analytics');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\DataTables\PromocodeDataTable;
+use App\Http\Controllers\Admin\AnalyticController;
 use App\Http\Controllers\Admin\BlogController;
 use App\Http\Controllers\Admin\BusinessSettingController;
 use App\Http\Controllers\Admin\CategoryController;
@@ -109,6 +110,14 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin', 'web'])->gr
     /==========================Promocode Management =====================>
     /=================================================================*/
     Route::name('promocodes.')->prefix('promocodes')->controller(PromocodeController::class)->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::post('/', 'store')->name('store');
+        Route::get('edit/{id}', 'edit')->name('edit');
+        Route::delete('destroy/{id}', 'destroy')->name('destroy');
+        Route::post('update', 'update')->name('update');
+        Route::put('status', 'status')->name('status');
+    });
+    Route::name('analytics.')->prefix('analytics')->controller(AnalyticController::class)->group(function () {
         Route::get('/', 'index')->name('index');
         Route::post('/', 'store')->name('store');
         Route::get('edit/{id}', 'edit')->name('edit');
