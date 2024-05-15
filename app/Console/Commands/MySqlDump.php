7258f204b9eca05c38625bd15b5af88d20ea2ae5
Raiyan <raiyan.appdid@gmail.com>
2024-05-15 12:33:16 +0530

    updated

--- app/Console/Commands/MySqlDump.php
+++ app/Console/Commands/MySqlDump.php
@@ -11,7 +11,7 @@ class MySqlDump extends Command
      *
      * @var string
      */
-    protected $signature = 'db:mysqldump {schema} {password}';
+    protected $signature = 'db:mysqldump {database_name} {password}';
 
     /**
      * The console command description.
@@ -29,11 +29,10 @@ class MySqlDump extends Command
     public function handle()
     {
         $ds = DIRECTORY_SEPARATOR;
-
-        $schema = $this->argument('schema');
-        $password = $this->argument('password');
+        $schema = $this->argument('database_name'); //your database name
+        $password = $this->argument('password'); //your database password
         $path = database_path() . $ds . 'backups' . $ds . date('Y') . $ds . date('m') . $ds;
-        $file = date('Y-m-d') . '_mysqldump.sql';
+        $file = date('d-m-Y') . '_mysqldump.sql';
         $command = sprintf('mysqldump %s -u raiyan -p\'%s\' > %s', $schema, $password, $path . $file);
         if (!is_dir($path)) {
             mkdir($path, 0755, true);
---
+++ app/Console/Commands/MySqlDumpBackupToS3.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class MySqlDumpBackupToS3 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:mysqlbackup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command excess the backup file created by db:mysqldump command and store that file into appdidZ digital ocean s3.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $ds = DIRECTORY_SEPARATOR;
+        $path = database_path() . $ds . 'backups' . $ds . date('Y') . $ds . date('m') . $ds . date('d-m-Y') . '_mysqldump.sql';
+        \Log::info(date('M-Y'));
+        if (file_exists($path)) {
+            $content = File::get($path);
+            $projectName = env('APP_NAME');
+            $url =  Storage::disk('do')->put('mysql-backups/' . $projectName . '/' . date('M-Y') . '/' . date('d-M-Y') . '_backup.sql', $content, 'private');
+            if ($url) {
+                File::delete($path);
+            }
+        } else {
+            \Log::info('file does not exists');
+        }
+        return Command::SUCCESS;
+    }
+}
